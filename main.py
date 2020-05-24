0b08a90c8c8697f5b92ef8c9bf2a1ec864cbb988
Veer Gadodia <veergadodia24@gmail.com>
2020-05-24 18:22:02 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import text_similarity
+
+def similarity_score(sentence1, sentence2):
+    percentage = text_similarity.token_set_ratio(sentence1, sentence2)
+    percentage = float(percentage)/100
+    return percentage
+
+sent1 = "THIS IS A REDESCRIPTION OF THE PARTY"
+sent2 = "THIS IS A DESCRIPTION"
+
+# print(similarity_score(sent1, sent2))
